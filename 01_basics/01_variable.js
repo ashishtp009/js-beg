e476a24ba7f99c85578847300d76d52eebbb6e1f
ashishtp009 <149559947+ashishtp009@users.noreply.github.com>
2023-11-01 10:42:36 +0000

    conversion of dataTypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 34567
+let accountEmail = "hitesh@google.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountstate;
+
+//accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "Banglore"
+console.table([accountId,accountstate,accountEmail,accountPassword]);
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+//let score = "33"
+
+//console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = "hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let somenumber = 33
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
